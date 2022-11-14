3b2ef19e7e5bb38b9ace8b7943b432043372e42b
James Fulford <james.fulford@outlook.com>
2022-11-14 17:49:31 -0500

    add way to list public symphonies

--- lib/symphony_object.py
+++ lib/symphony_object.py
@@ -5,20 +5,31 @@ import edn_format
 
 from . import edn_syntax
 
+COMPOSER_CONFIG = {
+    "projectId": "leverheads-278521",
+    "databaseName": "(default)"
+}
+
 
 def get_symphony(symphony_id: str) -> dict:
-    composerConfig = {
-        "projectId": "leverheads-278521",
-        "databaseName": "(default)"
-    }
+
     print(f"Fetching symphony {symphony_id} from Composer")
     response = requests.get(
-        f'https://firestore.googleapis.com/v1/projects/{composerConfig["projectId"]}/databases/{composerConfig["databaseName"]}/documents/symphony/{symphony_id}')
+        f'https://firestore.googleapis.com/v1/projects/{COMPOSER_CONFIG["projectId"]}/databases/{COMPOSER_CONFIG["databaseName"]}/documents/symphony/{symphony_id}')
     response.raise_for_status()
 
     response_json = response.json()
     return response_json
 
+
+def get_public_symphonies() -> list[dict]:
+    response = requests.get(
+        f"https://firestore.googleapis.com/v1/projects/{COMPOSER_CONFIG['projectId']}/databases/{COMPOSER_CONFIG['databaseName']}/documents/public_symphony/")
+    response.raise_for_status()
+
+    response_json = response.json()
+    print(f"fetched {len(response_json['documents'])} public symphonies")
+    return response_json['documents']
 
 def extract_root_node_from_symphony_response(response: dict) -> dict:
     return typing.cast(dict, edn_syntax.convert_edn_to_pythonic(
